2aead63df42ea5f176ae8f8b739b890bc835b046
Abdul Aziz <aziz@marensolutions.com>
2025-02-28 14:22:10 +0530

    obfuscated code with fix

--- scripts/obfuscate.js
+++ scripts/obfuscate.js
@@ -7,24 +7,19 @@ function obfuscateFile(filePath) {
     
     const obfuscationResult = JavaScriptObfuscator.obfuscate(code, {
         compact: true,
-        controlFlowFlattening: true,
-        controlFlowFlatteningThreshold: 0.75,
-        numbersToExpressions: true,
-        simplify: true,
-        stringArrayShuffle: true,
-        splitStrings: true,
-        stringArrayThreshold: 0.75,
-        deadCodeInjection: true,
-        deadCodeInjectionThreshold: 0.4,
-        debugProtection: true,
-        debugProtectionInterval: 2000,
-        disableConsoleOutput: true,
+        controlFlowFlattening: false,
+        deadCodeInjection: false,
+        debugProtection: false,
+        disableConsoleOutput: false,
         identifierNamesGenerator: 'hexadecimal',
         renameGlobals: false,
         rotateStringArray: true,
-        selfDefending: true,
+        selfDefending: false,
+        splitStrings: false,
         stringArray: true,
-        transform: null,
+        stringArrayEncoding: ['base64'],
+        stringArrayThreshold: 0.75,
+        transformObjectKeys: false,
         unicodeEscapeSequence: false
     });
 
